81c9a068196be222feb955335514382a8c1cfbce
Your Name <you@example.com>
2023-03-21 09:28:58 +0700

    TH2

---
+++ WBD4/TH2.js
@@ -0,0 +1,10 @@
+const http = require('http');
+http.createServer(function (req, res) {
+    let txt = ""
+    if(req.url === "/login") {
+        txt = "Login success";
+    } else {
+        txt = "Login fail";
+    }
+    res.end(txt);
+}).listen(8080, "localhost");
